6508826596c40a942a9b0ed317be889c04198496
Alexander Hmel <can4ik22@gmail.com>
2024-03-20 18:32:48 +0200

    feat: add dialog component

--- app.vue
+++ app.vue
@@ -22,16 +22,44 @@
       <UiInput class="max-w-[273px] mx-2 w-full" v-model="value" label="Test"/>
       <UiInput class="max-w-[273px] w-full" v-model="value" label="Test"/>
     </div>
+    <h2 class="mb-3 text-3xl mt-5 font-bold">Dialog</h2>
+    <div class="flex items-center w-full justify-center">
+      <UiButton class="max-w-[273px] w-full" title="Get my plan" bgColor="black" textColor="white" @click="handleClick"></UiButton>
+      <UiDialog v-model:visible="dialogVisible" size="w-[400px]">
+        <div class="flex flex-col items-center">
+          <div class="text-[21px] font-bold mb-5">
+            Payment method
+          </div>
+          <UiButton class="max-w-[304px] w-full mb-4" bgColor="amber-400" textColor="black" :hoverEffect="{bgColor: 'amber-300', borderColor: 'amber-300'}">
+            <div class="flex font-bold items-center text-[16px] justify-center uppercase">
+              <IconPaypal class="mr-2" />  Buy now
+            </div>
+          </UiButton>
+
+          <UiButton class="max-w-[304px] mx-2 w-full" bgColor="white" textColor="black" borderColor="gray-200" :hoverEffect="{bgColor: 'black', textColor: 'white', borderColor: 'black'}">
+            <div class="flex font-bold items-center text-[16px] justify-center uppercase">
+              <IconGooglePay class="mr-2" /> Pay
+            </div>
+          </UiButton>
+        </div>
+      </UiDialog>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue';
 import UiButton from './components/UI/button/ui-button.vue';
+import UiDialog from './components/UI/dialog/ui-dialog.vue';
 import IconGooglePay from './components/UI/icons/icon-google-pay.vue';
 import IconPaypal from './components/UI/icons/icon-paypal.vue';
 import UiInput from './components/UI/input/ui-input.vue';
 
 const value = defineModel({ default: 0 });
+const dialogVisible = ref(false);
+
+const handleClick = () => {
+  dialogVisible.value = true;
+};
 
 </script>
---
+++ components/UI/dialog/ui-dialog.vue
@@ -0,0 +1,102 @@
+<template>
+    <teleport to="body">
+        <Transition name="fade">
+        <div v-if="visible" class=" vsTailwind ui-dialog__wrapper">
+        <div class="ui-dialog w-full h-full fixed top-0 start-0 z-[80] bg-black bg-opacity-50">
+            <transition name="content" appear>
+            <div class="ui-dialog__modal opacity-1 transition-all flex flex-col bg-white border shadow-sm rounded-xl w-11/12" :class="formatedSize">
+                <div class="ui-dialog__content p-4">
+                    <slot />
+                </div>
+            </div>
+            </transition>
+        </div>
+        </div>
+        </Transition>
+    </teleport>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  visible: { type: Boolean, default: () => false },
+  title: { type: String, default: () => '' },
+  size: { type: String, default: () => '' }, // Size like width in tailwind
+});
+
+const formatedSize = computed(() => `lg:${props.size}`);
+
+</script>
+
+<style lang="scss" scoped>
+
+  .ui-dialog {
+    &__wrapper {
+      z-index: 10001;
+      position: relative;
+    }
+
+      &__modal {
+        margin: 10px;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        min-height: 0;
+        height: fit-content;
+        max-height: 80vh;
+        overflow: hidden;
+      }
+
+      &__content {
+          min-height: 0;
+          height: 100%;
+          overflow: auto;
+
+        &::-webkit-scrollbar {
+          width: 6px;
+          height: 6px;
+          background-color: #f5f5f5;
+        }
+        &::-webkit-scrollbar-thumb {
+          border-radius: 10px;
+          background-color: #d9d9d9bf;
+        }
+        &::-webkit-scrollbar-track {
+          background-color: #f5f5f5;
+        }
+      }
+  }
+
+  @media (max-width: 650px) {
+
+    .ui-dialog {
+      &__modal {
+        top: 50%;
+      }
+    }
+
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.2s;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .content-enter-active,
+  .content-leave-active {
+    transition: transform 0.4s;
+  }
+
+  .content-enter-from,
+  .content-leave-to {
+      transform: translate(-50%, -50%) scale(0.95);
+  }
+
+  </style>
