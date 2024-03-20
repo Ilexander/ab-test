dab598bf360156486db9665b4a2ba0d38d26a9f4
Alexander Hmel <can4ik22@gmail.com>
2024-03-20 18:43:51 +0200

    fix: app example

--- app.vue
+++ app.vue
@@ -30,17 +30,27 @@
           <div class="text-[21px] font-bold mb-5">
             Payment method
           </div>
-          <UiButton class="max-w-[304px] w-full mb-4" bgColor="amber-400" textColor="black" :hoverEffect="{bgColor: 'amber-300', borderColor: 'amber-300'}">
+          <UiButton class="w-full mb-4" bgColor="amber-400" textColor="black" :hoverEffect="{bgColor: 'amber-300', borderColor: 'amber-300'}">
             <div class="flex font-bold items-center text-[16px] justify-center uppercase">
               <IconPaypal class="mr-2" />  Buy now
             </div>
           </UiButton>
 
-          <UiButton class="max-w-[304px] mx-2 w-full" bgColor="white" textColor="black" borderColor="gray-200" :hoverEffect="{bgColor: 'black', textColor: 'white', borderColor: 'black'}">
+          <UiButton class="mx-2 w-full mb-3" bgColor="white" textColor="black" borderColor="gray-200" :hoverEffect="{bgColor: 'black', textColor: 'white', borderColor: 'black'}">
             <div class="flex font-bold items-center text-[16px] justify-center uppercase">
               <IconGooglePay class="mr-2" /> Pay
             </div>
           </UiButton>
+
+          <div class="relative w-full mb-4">
+            <div class="h-[1px] absolute z-1 top-2 bg-gray-300 w-full"></div>
+            <div class="text-center z-2 relative text-gray-400 font-semibold w-fit mx-auto px-2 text-[12px] bg-white">OR</div>
+          </div>
+          <UiInput class="w-full mb-4" v-model="value" label="Test"/>
+          <UiButton @click="handleClick" class="max-w-[273px] w-full font-bold" title="Submit" bgColor="gray-400" textColor="white"></UiButton>
+          <UiButton @click="handleClick" class="max-w-[273px] w-full font-bold" title="Close" bgColor="white" textColor="gray-400"
+          :hoverEffect="{bgColor: 'white', borderColor: 'white', textColor: 'gray-500'}"
+          :activeEffect="{bgColor: 'white', borderColor: 'white', textColor: 'gray-300'}"></UiButton>
         </div>
       </UiDialog>
     </div>
@@ -59,7 +69,7 @@ const value = defineModel({ default: 0 });
 const dialogVisible = ref(false);
 
 const handleClick = () => {
-  dialogVisible.value = true;
+  dialogVisible.value = !dialogVisible.value;
 };
 
 </script>
